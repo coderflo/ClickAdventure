cab915f8b0a3962142fb5ad14c8b52109c78e028
Florian Marwitz <flo.marwitz@gmail.com>
2016-02-06 11:39:45 +0100

    added basic project files

---
+++ src/unit1.pas
@@ -0,0 +1,26 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+
+type
+  TClickAdventure = class(TForm)
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  ClickAdventure: TClickAdventure;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
