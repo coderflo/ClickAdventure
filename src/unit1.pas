4f7796a417e53424c4b761705376d08e44ba36a4
Florian Marwitz <flo.marwitz@gmail.com>
2016-02-06 11:44:22 +0100

    added class declaration for TRoom

--- src/unit1.pas
+++ src/unit1.pas
@@ -15,12 +15,22 @@ type
     { public declarations }
   end;
 
+type
+  TRoom = class
+    // TODO: add field for backgroundImage
+    north,east,south,west:TRoom;
+    labelNorth,labelEast,labelSouth,labelWest:string;
+
+  end;
+
 var
   ClickAdventure: TClickAdventure;
 
 implementation
 
 {$R *.lfm}
+
+
 
 end.
 
