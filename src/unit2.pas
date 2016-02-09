334aca70536373e72a58b90156ca4f05fd723a40
Florian Marwitz <flo.marwitz@gmail.com>
2016-02-09 17:10:38 +0100

    added procedure: Unit2.addItem(string); added procedure: Unit2.clearItems()

--- src/unit2.pas
+++ src/unit2.pas
@@ -27,5 +27,15 @@ implementation
 
 {$R *.lfm}
 
+procedure clearItems();
+begin
+  BagView.itemStringList.Items.Clear;
+end;
+
+procedure addItem(name:string);
+begin
+  BagView.itemStringList.Items.Add(name);
+end;
+
 end.
 
